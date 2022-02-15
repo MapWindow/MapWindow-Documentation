ac1a7725d7fb7acd88171a84ffaa99b7ba533805
Paul Meems <bontepaarden@gmail.com>
2022-02-15 11:00:48 +0100

    First commit

---
+++ AxInterop.MapWinGIS/Enums.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable ArrangeAccessorOwnerBody
+// ReSharper disable DelegateSubtraction
+// ReSharper disable PossibleInvalidCastExceptionInForeachLoop
+// ReSharper disable CheckNamespace
+
+
+/// <summary>
+/// Justification of the labels
+/// </summary>
+public enum tkHJustification { }
+
+/// <summary>
+/// Shape drawing method
+/// </summary>
+public enum tkShapeDrawingMethod { }
+    
+
---
+++ Examples/StartForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Examples
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+
+            Shown += StartForm_Shown;
+        }
+
+        private void StartForm_Shown(object sender, EventArgs e)
+        {
+            using (var form = new TestForm())
+            {
+                form.ShowDialog(this);
+                form.Close();
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Close();
+        }
+    }
+}
