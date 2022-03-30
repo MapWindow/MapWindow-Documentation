658548806068892a8c601d7ef35d6409d3609e9a
Paul Meems <bontepaarden@gmail.com>
2022-03-30 16:46:07 +0200

    Small console app to generate HTML files to redirect to the new location

---
+++ src/CreateRedirectFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// List all files in the directory:
+// Process the list of files found in the directory.
+string[] fileEntries = Directory.GetFiles(@"D:\dev\MapWindow\MapWindow-Documentation\MapWinGIS\output\html", "*.html");
+foreach (string fileName in fileEntries)
+{
+    var baseName = Path.GetFileName(fileName);
+    Console.WriteLine(baseName);
+    var path = Path.Combine(@"D:\dev\MapWindow\MapWindow-Documentation\Website\documentation\mapwingis4.9", baseName);
+    using StreamWriter sw = File.CreateText(path);
+    sw.WriteLine("<!DOCTYPE HTML>");
+    sw.WriteLine("<html lang=\"en\">");
+    sw.WriteLine("    <head>");
+    sw.WriteLine("        <meta charset=\"utf-8\">");
+    sw.WriteLine("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+    sw.WriteLine($"        <meta http-equiv=\"refresh\" content=\"0; URL=https://www.mapwindow.org/mapwingis/{baseName}\" />");
+    sw.WriteLine($"        <title>MapWinGIS 4.9 Documentation - {Path.GetFileNameWithoutExtension(baseName)}</title>");
+    sw.WriteLine($"        <link rel=\"canonical\" href=\"https://www.mapwindow.org/mapwingis/{baseName}\" />");
+    sw.WriteLine("    </head>");
+    sw.WriteLine("    <body>");
+    sw.WriteLine("        <h1>");
+    sw.WriteLine($"            This page been moved to <a href=\"https://www.mapwindow.org/mapwingis/{baseName}\">www.mapwindow.org/mapwingis/{baseName}</a>");
+    sw.WriteLine("        </h1>");
+    sw.WriteLine("    </body>");
+    sw.WriteLine("</html>");
+}
+
